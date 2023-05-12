d4e08b558fd6f3fa6990e444f0b9d5e94b486584
PinkyPie <danilovich36misha@gmail.com>
2023-05-12 12:04:09 +0200

    First commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: false });
+
+  app.enableCors({ credentials: true, origin: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Coffee shop')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(7777);
+}
+bootstrap();
