6ecf5f21ba77478cfc25138a4e5cefcee6a689cf
PinkyPie <danilovich36misha@gmail.com>
2023-06-06 09:34:49 +0200

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -6,16 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://coffee-shop-doppio-ui.vercel.app/',
-    ],
-    allowedHeaders: ['Accept', 'Content-Type'],
-    preflightContinue: false,
-    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  });
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Coffee shop')
