42e46f97953d79a547372f866dc8a938177cd7a9
PinkyPie <danilovich36misha@gmail.com>
2023-06-09 12:49:46 +0200

    Remove comments

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // app.enableCors({
-  //   origin: true,
-  //   preflightContinue: false,
-  //   allowedHeaders:
-  //     'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: ['GET', 'POST', 'UPDATE', 'DELETE', 'PATCH', 'OPTIONS'],
-  //   credentials: true,
-  // });
-
   const config = new DocumentBuilder()
     .setTitle('Coffee shop')
     .setVersion('1.0')
